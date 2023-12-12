024093e1524ebc00f4fbf0991b939130ac404a04
rioseleng09 <94964007+rioseleng09@users.noreply.github.com>
2023-12-12 22:01:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-!pip install tensorflow
+pip install tensorflow
 from tensorflow.keras.models import model_from_json
 import pickle
 
